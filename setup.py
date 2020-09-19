2cf2d96a91e72313f056f3002be065b8a38a1e52
Miffka <miffka1992@inbox.ru>
2020-09-19 23:30:40 +0300

    [feature] Add setup.py
    * Make library out of this

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="py_sorting",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Sorting library",
+    author="",
+    license="",
+)
